81b9219b22fddda1e6d319f5e804518f525872f9
nonlinears-solutions <github@nonlinear.solutions>
2026-02-09 23:15:01 +0100

    removed dependcy for testing purposes

--- mix.exs
+++ mix.exs
@@ -68,8 +68,7 @@ defmodule Kanban.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
       {:bandit, "~> 1.5"},
-      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
-      {:decimal, "~> 2.3"}
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
     ]
   end
 
